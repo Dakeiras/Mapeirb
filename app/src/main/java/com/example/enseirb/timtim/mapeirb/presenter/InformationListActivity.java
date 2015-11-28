23f09db047c2c513cf058c68561dfef60ec54cc3
Arthur <a.fiot78@hotmail.fr>
2015-11-28 18:22:49 +0100

    correction du fragment de la map

--- app/src/main/java/com/example/enseirb/timtim/mapeirb/presenter/InformationListActivity.java
+++ app/src/main/java/com/example/enseirb/timtim/mapeirb/presenter/InformationListActivity.java
@@ -1,33 +1,27 @@
 package com.example.enseirb.timtim.mapeirb.presenter;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 import com.example.enseirb.timtim.mapeirb.R;
-import com.example.enseirb.timtim.mapeirb.business.IPOICollectionBusiness;
-import com.example.enseirb.timtim.mapeirb.business.POICollectionBusiness;
-import com.example.enseirb.timtim.mapeirb.business.listener.IPOICollectionBusinessListener;
-import com.example.enseirb.timtim.mapeirb.model.POI;
-import com.example.enseirb.timtim.mapeirb.model.POICollection;
-import com.example.enseirb.timtim.mapeirb.model.POIType;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class InformationListActivity extends FragmentActivity {
+public class InformationListActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final String SERVICE_NAME = "com.example.enseirb.timtim.mapeirb.presenter.SERVICE";
-
+    private GoogleMap map;
+    private List<LatLng> poiList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +33,12 @@ public class InformationListActivity extends FragmentActivity {
 
         InformationListFragment fragment = (InformationListFragment) getFragmentManager().findFragmentById(R.id.list_layout_fragment);
         fragment.createList(getIntent().getStringExtra(SERVICE_NAME));
+
+        SupportMapFragment mapFragment;
+        if((mapFragment= (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map)) != null) {
+            mapFragment.getMapAsync(this);
+        }
     }
 
     private void centerOnItem() {
@@ -53,4 +53,26 @@ public class InformationListActivity extends FragmentActivity {
         return intent;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng bdx = new LatLng(44.840950, -0.574813);
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(bdx, 12));
+
+        poiList.add(new LatLng(44.80, -0.574));
+        poiList.add(new LatLng(44.84, -0.57));
+        poiList.add(new LatLng(44.82, -0.594));
+        poiList.add(new LatLng(44.79, -0.525));
+
+        for (LatLng elm: poiList)
+            addMarker("cul", "coucou", elm);
+    }
+
+    private void addMarker(String title, String snippet, LatLng position){
+        map.addMarker(new MarkerOptions()
+                .title(title)
+                .snippet(snippet)
+                .position(position));
+    }
 }
