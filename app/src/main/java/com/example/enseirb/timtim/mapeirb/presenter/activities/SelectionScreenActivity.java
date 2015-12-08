0305c228652cdaba69927fcb599d067f78b9082a
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2015-12-08 22:38:29 +0100

    ajout d'une popup d'aide sur l'écran d'accueil. La position du bouton est tout ce qu'il y a de plus temporaire

--- app/src/main/java/com/example/enseirb/timtim/mapeirb/presenter/activities/SelectionScreenActivity.java
+++ app/src/main/java/com/example/enseirb/timtim/mapeirb/presenter/activities/SelectionScreenActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.enseirb.timtim.mapeirb.R;
+import com.example.enseirb.timtim.mapeirb.presenter.popupFactories.MsgPopupFactoryCancel;
 import com.example.enseirb.timtim.mapeirb.presenter.popupFactories.MsgPopupFactoryFinishApp;
 import com.example.enseirb.timtim.mapeirb.utils.ConnectivityChecker;
 
@@ -19,9 +20,27 @@ public class SelectionScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_selection_screen);
-        if(getResources().getBoolean(R.bool.portrait_only)){
+
+        if(getResources().getBoolean(R.bool.portrait_only))
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        }
+
+        initializePOIButtons();
+        displayPopupIfNotConnectedToInternet();
+        initHelpButton();
+    }
+
+    private void displayPopupIfNotConnectedToInternet(){
+        Context context = this;
+        if(!ConnectivityChecker.isDeviceConnected(context))
+            new MsgPopupFactoryFinishApp().show("Device not connected", "Please connect to the internet and try again.", context);
+    }
+
+    private void ShowList(String name) {
+        Intent intent = MapPresenterActivity.getIntent(this, name);
+        startActivity(intent);
+    }
+
+    private void initializePOIButtons(){
         Typeface font = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
         Typeface fontIcomoon = Typeface.createFromAsset(getAssets(), "icomoon.ttf");
 
@@ -61,20 +80,26 @@ public class SelectionScreenActivity extends AppCompatActivity {
                 ShowList(InformationListFragment.TOILET_NAME);
             }
         });
-        displayPopupIfNotConnectedToInternet();
+
     }
 
-    private void displayPopupIfNotConnectedToInternet(){
-        Context context = this;
-        if(!ConnectivityChecker.isDeviceConnected(context))
-            new MsgPopupFactoryFinishApp().show("Device not connected", "Please connect to the internet and try again.", context);
+    private void initHelpButton(){
+        Button helpButton = (Button) findViewById(R.id.content_selection_screen_help_button);
+
+        final Context activityContext = this;
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String helpContent = "This app allows you to locate useful points of interest in " +
+                        "Bordeaux.\n" +
+                        "Tap on one of these four button to display respectively :\n" +
+                        "- The electric car charging stations\n" +
+                        "- The public defibrilators\n" +
+                        "- The free WiFi hotspots\n" +
+                        "- The public restrooms";
+                new MsgPopupFactoryCancel().show("Help", helpContent, activityContext);
+            }
+        });
+
     }
-
-
-    private void ShowList(String name) {
-        Intent intent = MapPresenterActivity.getIntent(this, name);
-        startActivity(intent);
-    }
-
-
 }
